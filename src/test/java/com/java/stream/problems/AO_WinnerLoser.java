f4e0d06daae2e25141a1e2c7616cd2149ace8df4
Zahid_Khan <xahiidkhan@gmail.com>
2024-05-11 13:20:58 +0530

    Add more Stream Problems.

---
+++ src/test/java/com/java/stream/problems/AO_WinnerLoser.java
@@ -0,0 +1,13 @@
+package com.java.stream.problems;
+/*
+*
+
+
+100)
+	For a given string, like "aabbccdeff", count each char and declare winner or loser by following rules
+· Winner char will have max number of char count and no other char will have same count
+· Loser will have min number of char count and no other char will have same count.
+Input: aabbccdeff
+Output: winner: none, loser: e*/
+public class AO_WinnerLoser {
+}
