7c7fc83a75c6d2024dbb9710dd1c750e94d004b8
Zahid_Khan <xahiidkhan@gmail.com>
2024-05-11 13:30:05 +0530

    Add more Stream Problems.

--- src/test/java/com/java/stream/problems/Ak_ReArrangeElementInAscendingOrder.java
+++ src/test/java/com/java/stream/problems/Ak_ReArrangeElementInAscendingOrder.java
@@ -1,6 +1,9 @@
 package com.java.stream.problems;
 
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 /*
 * Q15 ) Given a numeric array , re arrange the elements to form a smallest possible value.
 
@@ -10,4 +13,9 @@ output is: 133444576998
 
 Solution is: Arrays.stream(arr).mapToObj(i-> i+"").sorted().forEach(System.out::print);*/
 public class Ak_ReArrangeElementInAscendingOrder {
+
+    @Test
+    public void test(){
+        Assertions.fail();
+    }
 }
