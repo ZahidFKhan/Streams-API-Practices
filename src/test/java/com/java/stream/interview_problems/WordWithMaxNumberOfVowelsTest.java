cb084268b40a299e14f4ba9dbdbf9ca2fe39d6d7
Zahid_Khan <xahiidkhan@gmail.com>
2024-05-12 11:04:17 +0530

    Add test cases capability.

--- src/test/java/com/java/stream/interview_problems/WordWithMaxNumberOfVowelsTest.java
+++ src/test/java/com/java/stream/interview_problems/WordWithMaxNumberOfVowelsTest.java
@@ -1,16 +1,28 @@
 package com.java.stream.interview_problems;
 
+import com.github.javafaker.Faker;
+import com.java.stream.solutions.InterviewProblemSolutions;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 
 /*
- *  Given a string,
- *  Find the word with the highest number of vowels.
+ * Given a string,
+ * Find the highest number of vowels present in a word.
+ *
+ * Example:
+ * INPUT: HELLO HOW ARE YOU
+ * OUTPUT: 2
+ * EXPLANATION: HELLO contains 2 vowels.
  * */
 public class WordWithMaxNumberOfVowelsTest {
 
   @Disabled
+  @Test
   void testNumberOfVowels() {
-    var input = "Hello, My name is X";
-    //        Assertions.assertEquals(InterviewProblemSolutions.);
+    var input = Faker.instance().chuckNorris().fact();
+    final var expected = InterviewProblemSolutions.maxNumberOfVowels(input);
+    final var actual = 0;
+    Assertions.assertEquals(expected, actual);
   }
 }
--- src/test/java/com/java/stream/solutions/InterviewProblemSolutions.java
+++ src/test/java/com/java/stream/solutions/InterviewProblemSolutions.java
@@ -1,6 +1,8 @@
 package com.java.stream.solutions;
 
 import com.java.stream.interview_problems.domain.Employee;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -12,5 +14,15 @@ public class InterviewProblemSolutions {
         .filter(x -> x.email().split("@").length == 2)
         .map(x -> x.email().split("@")[1])
         .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+  }
+
+  public static Integer maxNumberOfVowels(String input) {
+    final var i =
+        Arrays.stream(input.split(" "))
+            .map(x -> x.length() - x.replaceAll("[aeiouAEIOU]", "").length())
+            .max(Comparator.comparing(Integer::intValue))
+            .get();
+
+    return i;
   }
 }
