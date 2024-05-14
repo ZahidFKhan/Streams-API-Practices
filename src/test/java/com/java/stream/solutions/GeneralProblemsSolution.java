4364b36288d1b724d62064ddab86c5c78ca3f25d
Zahid_Khan <xahiidkhan@gmail.com>
2024-05-14 11:32:46 +0530

    Add Max Projects practice

--- src/test/java/com/java/stream/solutions/GeneralProblemsSolution.java
+++ src/test/java/com/java/stream/solutions/GeneralProblemsSolution.java
@@ -1,5 +1,14 @@
 package com.java.stream.solutions;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class GeneralProblemsSolution {
 
+  public static List<String> sortListBasedOn2ndCharacter(List<String> input) {
+    return input.stream()
+        .sorted(Comparator.comparing(e -> e.charAt(1)))
+        .collect(Collectors.toList());
+  }
 }
