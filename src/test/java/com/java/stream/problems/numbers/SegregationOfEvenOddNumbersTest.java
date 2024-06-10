c56866ba8d4922d9ea69bc9e9e65543bd6f85fb9
Zahid_Khan <xahiidkhan@gmail.com>
2024-06-10 21:15:47 +0530

    Add SumOfUniqueDigitsTest.java

--- src/test/java/com/java/stream/problems/numbers/SegregationOfEvenOddNumbersTest.java
+++ src/test/java/com/java/stream/problems/numbers/SegregationOfEvenOddNumbersTest.java
@@ -1,6 +1,16 @@
 package com.java.stream.problems.numbers;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
 /*
  * Given a list of integers, divide into two lists one having even numbers and other having odd numbers.
  */
-public class SegregationOfEvenOddNumbersTest {}
+public class SegregationOfEvenOddNumbersTest {
+    @Test
+    @Disabled
+      void testSegregationOfEvenOddNumbersTest(){
+
+      }
+
+}
