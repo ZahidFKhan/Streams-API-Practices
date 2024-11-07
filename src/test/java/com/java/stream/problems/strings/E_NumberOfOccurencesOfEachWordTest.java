0fa2fd4d02f919af0346c01ef02c0dbbc6afc9f0
Zahid_Khan <xahiidkhan@gmail.com>
2024-11-07 20:59:52 +0530

    Add logic to E_NumberOfOccurencesOfEachWordTest.java

--- src/test/java/com/java/stream/problems/strings/E_NumberOfOccurencesOfEachWordTest.java
+++ src/test/java/com/java/stream/problems/strings/E_NumberOfOccurencesOfEachWordTest.java
@@ -10,29 +10,32 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class E_NumberOfOccurencesOfEachWordTest {
-    @Test
-    @Disabled("Remove This Once you Complete The Exercise")
-    public void numberOfOccurencesOfEachWord() {
-        String input = "the quick brown fox jumps right over the little lazy dog little";
-        final Map<String, Long> mySolution = Map.ofEntries(Map.entry("over", 1L),
-                Map.entry("the", 2L),
-                Map.entry("quick", 1L),
-                Map.entry("lazy", 1L),
-                Map.entry("jumps", 1L),
-                Map.entry("right", 1L),
-                Map.entry("brown", 1L),
-                Map.entry("dog", 1L),
-                Map.entry("fox", 1L),
-                Map.entry("little", 2L));
+  @Test
+  @Disabled
+  public void numberOfOccurencesOfEachWord() {
+    final var input = "the quick brown fox jumps right over the little lazy dog little";
+    final Map<String, Long> mySolution =
+        Map.ofEntries(
+            Map.entry("over", 1L),
+            Map.entry("the", 2L),
+            Map.entry("quick", 1L),
+            Map.entry("lazy", 1L),
+            Map.entry("jumps", 1L),
+            Map.entry("right", 1L),
+            Map.entry("brown", 1L),
+            Map.entry("dog", 1L),
+            Map.entry("fox", 1L),
+            Map.entry("little", 2L));
 
-        String yourSolution = "<PUT YOUR LOGIC HERE>";
-        // Assert that the yourSolution output matches the mySolution output
-        assertEquals(mySolution, yourSolution);
-    }
+    String yourSolution = "";
+    assertEquals(mySolution, yourSolution);
+  }
 
-    private void solution1() {
-        String input = "the quick brown fox jumps right over the little lazy dog little";
-        Map<String, Long> collect = Arrays.stream(input.split(" ")).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-        System.out.println(collect);
-    }
+  private void solution1() {
+    String input = "the quick brown fox jumps right over the little lazy dog little";
+    Map<String, Long> collect =
+        Arrays.stream(input.split(" "))
+            .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+    System.out.println(collect);
+  }
 }
