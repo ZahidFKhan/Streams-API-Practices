56a5a309063ba7f062225fcb678201046060d71f
Zahid_Khan <xahiidkhan@gmail.com>
2024-11-15 10:28:38 +0530

    Add logic to K_FindDuplicateStrings.java

--- src/test/java/com/java/stream/interview/B_FirstNonRepeatingCharacterTest.java
+++ src/test/java/com/java/stream/interview/B_FirstNonRepeatingCharacterTest.java
@@ -15,7 +15,8 @@ class B_FirstNonRepeatingCharacterTest {
 
   @Test
   @Disabled
-  void testFirstNonRepeatingCharacter() {    final var input =
+  void testFirstNonRepeatingCharacter() {
+    final var input =
         "The quick brown fox jumps over the lazy dog, find the first non repeated character.";
     final var yourSolution = ' ';
     final var mySolution = InterviewProblemSolutions.firstNonRepeatingCharacter(input);
