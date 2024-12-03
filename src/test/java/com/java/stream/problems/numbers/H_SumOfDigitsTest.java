79f4d9f006af6ff46c86db16dcf5f3306284f1b6
Zahid_Khan <xahiidkhan@gmail.com>
2024-12-03 11:59:52 +0530

    Refactor the classes and rename the methods.

--- src/test/java/com/java/stream/problems/numbers/H_SumOfDigitsTest.java
+++ src/test/java/com/java/stream/problems/numbers/H_SumOfDigitsTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-/*
+/**
  * Given an array of {5,6,7,8,5,5,8,8,7)
  * Find the sum of the unique elements.
  * The output should be in this case is: 26.
