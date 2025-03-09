3933e220df91219471ac1c182cbfe6905e64d180
Zahid_Khan <xahiidkhan@gmail.com>
2025-03-09 11:31:12 +0530

    Fix module-info.java

--- src/test/java/module-info.java
+++ src/test/java/module-info.java
@@ -1,24 +1,30 @@
 module com.github.streams {
   requires org.junit.jupiter;
+  requires org.junit.platform.commons;
   requires javafaker;
 
+  //  INTERVIEW
   opens com.github.streams.interview.problems.numbers to
       org.junit.platform.commons;
   opens com.github.streams.interview.problems.strings to
       org.junit.platform.commons;
+  opens com.github.streams.interview.problems.employee to
+      org.junit.platform.commons;
+  opens com.github.streams.interview to
+      org.junit.platform.commons;
+
+  //  LEARN
   opens com.github.streams.learn.content to
       org.junit.platform.commons;
   opens com.github.streams.learn.content.functional_interfaces to
       org.junit.platform.commons;
+  opens com.github.streams.learn.solutions to
+      org.junit.platform.commons;
+
+  //  PRACTICE
   opens com.github.streams.practice.strings to
       org.junit.platform.commons;
   opens com.github.streams.practice.numbers to
-      org.junit.platform.commons;
-  opens com.github.streams.learn.solutions to
-      org.junit.platform.commons;
-  opens com.github.streams.interview.problems.employee to
-      org.junit.platform.commons;
-  opens com.github.streams.interview to
       org.junit.platform.commons;
   opens com.github.streams.practice.numbers.problems to
       org.junit.platform.commons;
