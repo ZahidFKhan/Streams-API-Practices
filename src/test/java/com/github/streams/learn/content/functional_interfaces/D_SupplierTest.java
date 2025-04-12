1ebbda400681697983d7d7a9ac22bd2c3a013f41
gotaradu <gotaradu6@gmail.com>
2025-04-12 18:03:11 +0300

    supplier

--- src/test/java/com/github/streams/learn/content/functional_interfaces/D_SupplierTest.java
+++ src/test/java/com/github/streams/learn/content/functional_interfaces/D_SupplierTest.java
@@ -3,34 +3,38 @@ package com.github.streams.learn.content.functional_interfaces;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.function.Supplier;
+
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class D_SupplierTest {
-  /** Write a lambda expression that returns a new StringBuilder containing the string "abc". */
-  @Test
-  @Disabled
-  public void d_supplier1() {
-    Supplier<StringBuilder> sup = null; // TODO
+    /**
+     * Write a lambda expression that returns a new StringBuilder containing the string "abc".
+     */
+    @Test
+    public void d_supplier1() {
+        Supplier<StringBuilder> sup = null; // TODO
+        sup = () -> new StringBuilder("abc");
+        assertEquals("abc", sup.get().toString());
+    }
 
-    assertEquals("abc", sup.get().toString());
-  }
+    /**
+     * Write a lambda expression that returns a new, empty StringBuilder.
+     */
+    @Test
+    public void d_supplier2() {
+        Supplier<StringBuilder> sup = null; // TODO
+        sup = () -> new StringBuilder();
+        assertEquals("", sup.get().toString());
+    }
 
-  /** Write a lambda expression that returns a new, empty StringBuilder. */
-  @Test
-  @Disabled
-  public void d_supplier2() {
-    Supplier<StringBuilder> sup = null; // TODO
-
-    assertEquals("", sup.get().toString());
-  }
-
-  /** Write a constructor reference that returns a new, empty StringBuilder. */
-  @Test
-  @Disabled
-  public void d_supplier3() {
-    Supplier<StringBuilder> sup = null; // TODO
-
-    assertEquals("", sup.get().toString());
-  }
+    /**
+     * Write a constructor reference that returns a new, empty StringBuilder.
+     */
+    @Test
+    public void d_supplier3() {
+        Supplier<StringBuilder> sup = null; // TODO
+        sup = StringBuilder::new;
+        assertEquals("", sup.get().toString());
+    }
 }
