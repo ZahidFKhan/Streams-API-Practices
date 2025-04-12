ba349b29493900a9f39e8db8d5de29b421102cfc
gotaradu <gotaradu6@gmail.com>
2025-04-12 15:39:24 +0300

    consumer

--- src/test/java/com/github/streams/learn/content/functional_interfaces/C_ConsumerTest.java
+++ src/test/java/com/github/streams/learn/content/functional_interfaces/C_ConsumerTest.java
@@ -5,58 +5,69 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class C_ConsumerTest {
-  /** Write a lambda expression that appends the string "abc" to the given StringBuilder. */
-  @Test
-  @Disabled
-  public void c_consumer1() {
-    Consumer<StringBuilder> cons = null; // TODO
+    /**
+     * Write a lambda expression that appends the string "abc" to the given StringBuilder.
+     */
+    @Test
+    public void c_consumer1() {
+        Consumer<StringBuilder> cons = null; // TODO
 
-    StringBuilder sb = new StringBuilder("xyz");
-    cons.accept(sb);
-    assertEquals("xyzabc", sb.toString());
-  }
 
-  /** Write a lambda expression that clears the given list. */
-  @Test
-  @Disabled
-  public void c_consumer2() {
-    Consumer<List<String>> cons = null; // TODO
+        StringBuilder sb = new StringBuilder("xyz");
+        cons = s -> s.append("abc");
+        cons.accept(sb);
+        assertEquals("xyzabc", sb.toString());
+    }
 
-    List<String> list = new ArrayList<>(List.of("a", "b", "c"));
-    cons.accept(list);
-    Assertions.assertTrue(list.isEmpty());
-  }
+    /**
+     * Write a lambda expression that clears the given list.
+     */
+    @Test
+    public void c_consumer2() {
+        Consumer<List<String>> cons = null; // TODO
 
-  /** Write an unbound method reference that clears the given list. */
-  @Test
-  @Disabled
-  public void c_consumer3() {
-    Consumer<List<String>> cons = null; // TODO
+        List<String> list = new ArrayList<>(List.of("a", "b", "c"));
+        cons = (l) -> l.clear();
+        cons.accept(list);
+        Assertions.assertTrue(list.isEmpty());
+    }
 
-    List<String> list = new ArrayList<>(List.of("a", "b", "c"));
-    cons.accept(list);
-    Assertions.assertTrue(list.isEmpty());
-  }
+    /**
+     * Write an unbound method reference that clears the given list.
+     */
+    @Test
+    public void c_consumer3() {
+        Consumer<List<String>> cons = null; // TODO
 
-  /**
-   * Given two consumers, create a consumer that passes the String to the first consumer, then to
-   * the second.
-   */
-  @Test
-  @Disabled
-  public void c_consumer4() {
-    Consumer<List<String>> c1 = list -> list.add("first");
-    Consumer<List<String>> c2 = list -> list.add("second");
+        List<String> list = new ArrayList<>(List.of("a", "b", "c"));
 
-    Consumer<List<String>> consumer = null; // TODO
+        cons = List::clear;
 
-    List<String> list = new ArrayList<>(List.of("a", "b", "c"));
-    consumer.accept(list);
-    assertEquals(List.of("a", "b", "c", "first", "second"), list);
-  }
+        cons.accept(list);
+        Assertions.assertTrue(list.isEmpty());
+    }
+
+    /**
+     * Given two consumers, create a consumer that passes the String to the first consumer, then to
+     * the second.
+     */
+    @Test
+    public void c_consumer4() {
+        Consumer<List<String>> c1 = list -> list.add("first");
+        Consumer<List<String>> c2 = list -> list.add("second");
+
+        Consumer<List<String>> consumer = null; // TODO
+
+        consumer = s -> c1.andThen(c2).accept(s);
+
+        List<String> list = new ArrayList<>(List.of("a", "b", "c"));
+        consumer.accept(list);
+        assertEquals(List.of("a", "b", "c", "first", "second"), list);
+    }
 }
