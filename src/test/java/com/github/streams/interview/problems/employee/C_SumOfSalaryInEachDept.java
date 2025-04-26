a90a99bbd7e56a9e4cd5d6a4d2e5498964ebf90d
Zahid_Khan <xahiidkhan@gmail.com>
2025-04-26 10:05:23 +0530

    Add documentation to B_MaxSalaryInEachDept.java

--- src/test/java/com/github/streams/interview/problems/employee/C_SumOfSalaryInEachDept.java
+++ src/test/java/com/github/streams/interview/problems/employee/C_SumOfSalaryInEachDept.java
@@ -8,16 +8,17 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 /**
- * Calculation of the sum of salaries in each department.<br/>
+ * Calculation of the sum of salaries in each department.<br>
  *
  * <h2>Input:</h2>
  *
- * <li>Employee("John", "Sales", 50000)</li>
- * <li>Employee("Jane", "Marketing", 60000)</li>
- * <li>Employee("Bob", "Sales", 70000)</li>
- *   <br/>
- * <h2>Output:</h2>
- *   <pre>
+ * <li>Employee("John", "Sales", 50000)
+ * <li>Employee("Jane", "Marketing", 60000)
+ * <li>Employee("Bob", "Sales", 70000) <br>
+ *
+ *     <h2>Output:</h2>
+ *
+ *     <pre>
  *   {
  *     "Sales": 120000,
  *     "Marketing": 60000
