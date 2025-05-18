f13a1a49b21a2804c3fc44e9068c9bd74764eadb
Zahid_Khan <xahiidkhan@gmail.com>
2025-05-18 15:58:21 +0530

    Add documentation to F_ManagerWithMaxEmployees.java

--- src/test/java/com/github/streams/interview/problems/employee/G_EmployeesWhoWorkedOnMostProject.java
+++ src/test/java/com/github/streams/interview/problems/employee/G_EmployeesWhoWorkedOnMostProject.java
@@ -9,40 +9,71 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 /**
- * 1. You are given a list of employees. 2. Each employee has an id, projectName, and a list of
- * projects they have worked on. 3. Each project has a projectName and projectDurationInMonths in
- * months.
+ * Finds the employee who has worked on the most projects.
  *
- * <p>find the employee who has worked on the most projects using Streams API
+ * <p>If there are multiple employees with the same maximum number of projects, returns the employee
+ * with the longest total project duration in months. If there are still ties, returns any one of
+ * the tied employees.
  *
- * <p>If there are multiple employees with the same maximum number of projects,
+ * <p>Example:
  *
- * <p>return the employee with the longest total projectDurationInMonths of projects. If there are
- * still ties, return any one of the tied employees.
+ * <pre>
+ * Input: [
+ *   Employee(1, "John", [
+ *     Project("Project A", 3),
+ *     Project("Project B", 2)
+ *   ]),
+ *   Employee(2, "Alice", [
+ *     Project("Project C", 4),
+ *     Project("Project D", 1)
+ *   ]),
+ *   Employee(3, "Bob", [
+ *     Project("Project E", 2),
+ *     Project("Project F", 3),
+ *     Project("Project G", 1)
+ *   ])
+ * ]
+ * <br>
+ *
+ * Output: Employee(3, "Bob") (since Bob has worked on 3 projects with a total duration of 6 months)
+ * </pre>
  */
 class G_EmployeesWhoWorkedOnMostProject {
   @Test
   @Disabled()
   public void employeeWorkedOnMostProject() {
-    final var projectsThatEmp1HasWorkedOn = DummyProjects.listOfProjects1();
-    final var projectsThatEmp2HasWorkedOn = DummyProjects.listOfProjects2();
+    // Given
+    final var employee1 = new Identity("employee1@gmail.com", "Xee");
+    final var employee2 = new Identity("employee2@yahoo.com", "Shee");
 
-    final var employee1 = new Identity("x@y.z", "employee1");
-    final var employee2 = new Identity("y@a.z", "employee2");
+    final var employee1ProjectsHistory = DummyProjects.listOfProjects1();
+    final var employee2ProjectsHistory = DummyProjects.listOfProjects2();
 
     var employees = new ArrayList<Employee>();
-    employees.add(
-        new Employee(
-            1, employee1, 10000, Department.ComputerScience, projectsThatEmp1HasWorkedOn, -1));
+    addEmployees(
+        employees, employee1, employee1ProjectsHistory, employee2, employee2ProjectsHistory);
 
-    employees.add(
-        new Employee(
-            1, employee2, 10000, Department.ComputerScience, projectsThatEmp2HasWorkedOn, -1));
-
+    // When
     var mySolution = InterviewProblemSolutions.employeesWorkedForMaxProjects(employees);
 
+    // Then
     Employee yourSolution = null;
 
     Assertions.assertEquals(mySolution, yourSolution);
+  }
+
+  private static void addEmployees(
+      ArrayList<Employee> employees,
+      Identity employee1,
+      Collection<Project> employee1ProjectsHistory,
+      Identity employee2,
+      Collection<Project> employee2ProjectsHistory) {
+    employees.add(
+        new Employee(
+            1, employee1, 10000, Department.ComputerScience, employee1ProjectsHistory, -1));
+
+    employees.add(
+        new Employee(
+            2, employee2, 10000, Department.ComputerScience, employee2ProjectsHistory, -1));
   }
 }
