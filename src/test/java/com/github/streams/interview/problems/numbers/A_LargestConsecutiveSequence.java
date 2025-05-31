d55508fc9a3b6c9dc2e7f7d2411dfbf217e39435
Zahid_Khan <xahiidkhan@gmail.com>
2025-05-31 13:10:35 +0530

    Markdown Documentation Comments

--- src/test/java/com/github/streams/interview/problems/numbers/A_LargestConsecutiveSequence.java
+++ src/test/java/com/github/streams/interview/problems/numbers/A_LargestConsecutiveSequence.java
@@ -6,23 +6,31 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-/**
- * Given an unsorted array of integers nums, return the length of the longest consecutive elements
- * sequence. <br>
- * You must write an algorithm that runs in O(n) time. <br>
- * Example 1: <br>
- * Input: nums = [100,4,200,1,3,2]<br>
- * Output: 4<br>
- * Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is
- * 4.<br>
- * <br>
- * <br>
- * Example 2:<br>
- * <br>
- * Input: nums = [0,3,7,2,5,8,4,6,0,1]<br>
- * Output: 9<br>
- * <br>
- */
+/// ### Longest Consecutive Sequence
+///
+/// Given an unsorted array of integers `nums`, return the length of the longest consecutive elements
+/// sequence.
+///
+/// You must write an algorithm that runs in $O(n)$ time.
+///
+/// ### Example 1:
+///
+/// ```
+/// Input: nums = [100, 4, 200, 1, 3, 2]
+/// ```
+/// ```
+/// Output: 4
+/// ```
+/// Explanation: The longest consecutive elements sequence is `[1, 2, 3, 4]`. Therefore its length is 4.
+///
+/// ### Example 2:
+///
+/// ```
+/// Input: nums = [0, 3, 7, 2, 5, 8, 4, 6, 0, 1]
+/// ```
+/// ```
+/// Output: 9
+/// ```
 class A_LargestConsecutiveSequence {
   @Test
   @Disabled
